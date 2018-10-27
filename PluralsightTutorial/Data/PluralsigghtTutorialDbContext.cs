b9da780bd020f4e65cb43717a84dd48e85121ec7
tslomczewski <tslomczewski@gmail.com>
2018-10-27 18:34:13 +0200

    added database

---
+++ PluralsightTutorial/Data/PluralsigghtTutorialDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PluralsightTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluralsightTutorial.Data
+{
+    public class PluralsigghtTutorialDbContext : DbContext
+    {
+        public PluralsigghtTutorialDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurant { get; set; }
+    }
+}
---
+++ PluralsightTutorial/Services/SqlRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PluralsightTutorial.Data;
+using PluralsightTutorial.Models;
+
+namespace PluralsightTutorial.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private PluralsigghtTutorialDbContext _context;
+
+        public SqlRestaurantData(PluralsigghtTutorialDbContext context)
+        {
+            _context = context;
+        }
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurant.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurant.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurant.OrderBy(r => r.Name);
+        }
+    }
+}
